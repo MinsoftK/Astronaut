ae4b34fdc3ee9b984ea796636442add6ff03f5e4
Minsung Kim <dolikart@naver.com>
2021-08-01 23:02:19 +0900

    news List 제작

--- js/news.js
+++ js/news.js
@@ -1,4 +1,4 @@
-const newsIcon = document.querySelector('#news .news__btn');
+const newsIcon = document.querySelector('newsListBlock');
 const NEWS_API_KEY = 'b296d7212b79483d8d9813d53fa04244';
 
 console.log(newsIcon);
